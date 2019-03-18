84feaf69ccd637fccc2b695fa3fdba5c348f9a0b
daveluo <kitsudog@gmail.com>
2019-03-18 20:09:51 +0800

    补上income目录的映射

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # 为click框架初始化一下
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-if [ "${PASS_REQUIREMENTS:-FALSE}" == "FALSE" ]
+if [[ "${PASS_REQUIREMENTS:-FALSE}" == "FALSE" ]]
 then
     # 这个优化是给内网环境准备的
     pip3 install --trusted-host mirrors.aliyun.com -i http://mirrors.aliyun.com/pypi/simple -r requirements.txt
@@ -11,11 +11,12 @@ fi
 # suit 打补丁
 function patch_suit(){
     base=`python3.6 -c 'import suit,os;print(os.path.dirname(suit.__file__))'`
-    sed -i 's/from django.core.urlresolvers /from django.urls /g' $base/templatetags/suit_tags.py
-    sed -i 's/@register.assignment_tag/@register.simple_tag/g' $base/templatetags/suit_tags.py
-    sed -i 's/from django.core.urlresolvers /from django.urls /g' $base/templatetags/suit_menu.py
-    sed -i 's/@register.assignment_tag/@register.simple_tag/g' $base/templatetags/suit_menu.py
+    sed -i 's/from django.core.urlresolvers /from django.urls /g' ${base}/templatetags/suit_tags.py
+    sed -i 's/@register.assignment_tag/@register.simple_tag/g' ${base}/templatetags/suit_tags.py
+    sed -i 's/from django.core.urlresolvers /from django.urls /g' ${base}/templatetags/suit_menu.py
+    sed -i 's/@register.assignment_tag/@register.simple_tag/g' ${base}/templatetags/suit_menu.py
 }
+
 patch_suit
 
 python3.6 manage.py collectstatic --no-input
@@ -25,7 +26,9 @@ python3.6 manage.py migrate
 # todo: celery -B 只用于debug模式
 # nohup celery worker -A loop -B --loglevel INFO --logfile /var/log/server/celery.log &
 
-if [ ${UWSGI:-FALSE} = "TRUE" -o ${VIRTUAL_PROTO:-http} = "uwsgi" ]
+ln -s /var/www/income /app/server/static/income
+
+if [[ ${UWSGI:-FALSE} = "TRUE" || ${VIRTUAL_PROTO:-http} = "uwsgi" ]]
 then
     uwsgi --socket :8000 --gevent --gevent-monkey-patch --module ios_ci.wsgi  --async 100 --http-keepalive --chmod-socket=664
 else
